4aa837bc570c9ddd6dd93c806bd5b2717600a516
Prithvi Rao Ratti <prithvimagic@gmail.com>
2021-11-20 20:26:17 +0530

    Total Project commit

---
+++ Fructus/Fructus/App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Fructus
+//
+//  Created by Ratti on 05/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        OnboardingView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ Fructus/Fructus/App/FructusApp.swift
@@ -0,0 +1,16 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ratti on 05/09/21.
+//
+
+import SwiftUI
+
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup{
+            OnboardingView()
+        }
+    }
+}
